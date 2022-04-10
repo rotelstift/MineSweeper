4e079f7652a7af5888e50a6dc9b2e371a1c906a6
rotelstift <rotelstift@gmail.com>
2022-04-10 18:08:49 +0900

    init game field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GameFieldComponent } from './game-field/game-field.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameFieldComponent
   ],
   imports: [
     BrowserModule
